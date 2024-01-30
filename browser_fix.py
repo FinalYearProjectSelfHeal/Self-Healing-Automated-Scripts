9cb66135f678bfd42c47e36584b987162e0a05ae
SunilSSamra <128624112+SunilSSamra@users.noreply.github.com>
2024-01-30 12:27:31 +0000

    Update browser_fix.py
    Uses shadow roots to locate elements
    Unticks the browser history clear checkbox

--- browser_fix.py
+++ browser_fix.py
@@ -2,7 +2,10 @@ import subprocess
 import time, logging, psutil, requests
 import undetected_chromedriver as webdriver
 from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium import webdriver as selenium_webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 from osquery_agent import OSQueryAgent
 from bs4 import BeautifulSoup as bs
@@ -17,6 +20,8 @@ class BrowserFix:
         self.browser = "Google Chrome"
         self.osquery_agent = OSQueryAgent()
         self.notification = Notification()
+        self.selenium_webdriver = ""
+        self.selenium_options = selenium_webdriver.ChromeOptions()
 
 
     def close_browser(self):
@@ -47,30 +52,54 @@ class BrowserFix:
 
         return False
 
-    def perform_actions(self, keys: str):
-        actions = ActionChains(self.driver)
-        actions.send_keys(keys)
-        time.sleep(2)
-        print("actions ran")
-        actions.perform()
-
     def delete_cookies_cache(self):
         browser_running = self.check_browser_not_running()
         print(browser_running, "check")
 
         if not browser_running:
-            print("does this run")
-            user_data_dir = r"/Users/sunilsamra/Library/Application Support/Google/Chrome/Profile 2"
+            try:
+                print("does this run")
+                user_data_dir = r"/Users/sunilsamra/Library/Application Support/Google/Chrome/Profile 2"
 
-            self.options.add_argument(f"user-data-dir={user_data_dir}")
-            self.options.add_argument(r'--profile-directory=Default')
-            self.driver = webdriver.Chrome(options=self.options, use_subprocess=True)
-            self.driver.get("chrome://settings/clearBrowserData")
-            self.driver.implicitly_wait(2)
+                self.options.add_argument(f"user-data-dir={user_data_dir}")
+                self.options.add_argument(r'--profile-directory=Default')
 
-            self.perform_actions(Keys.TAB * 7 + Keys.ENTER + Keys.TAB * 6 + Keys.ENTER)
-            time.sleep(30)
-            self.driver.close()
+                self.selenium_options.add_argument(f"user-data-dir={user_data_dir}")
+                self.selenium_options.add_argument(r'--profile-directory=Default')
+
+                self.selenium_webdriver = webdriver.Chrome(options=self.selenium_options)
+                self.selenium_webdriver.get("chrome://settings/clearBrowserData")
+                time.sleep(5)
+
+                clear_data_button_script = """return document.querySelector("body > settings-ui").shadowRoot.querySelector("#main").shadowRoot.querySelector(
+                    "settings-basic-page").shadowRoot.querySelector(
+                    "#basicPage > settings-section:nth-child(11) > settings-privacy-page").shadowRoot.querySelector(
+                    "settings-clear-browsing-data-dialog").shadowRoot"""
+
+                checkboxes_script = """return document.querySelector("body > settings-ui").shadowRoot.querySelector("#main").
+                shadowRoot.querySelector("settings-basic-page").shadowRoot.querySelector("#basicPage > settings-section:nth-child(11) > settings-privacy-page").
+                shadowRoot.querySelector("settings-clear-browsing-data-dialog").shadowRoot.querySelector("#browsingCheckboxBasic").shadowRoot.querySelector("#checkbox").
+                shadowRoot"""
+
+                clear_data_button_shadow_root = self.selenium_webdriver.execute_script(clear_data_button_script)
+                checkboxes_shadow_root = self.selenium_webdriver.execute_script(checkboxes_script)
+
+                clear_data_button = clear_data_button_shadow_root.find_element(By.ID, "clearBrowsingDataConfirm")
+
+                # Ensure the browsing history is not cleared
+                browsing_history_checkbox = checkboxes_shadow_root.find_element(By.ID, "checkbox")
+                # Un-tick the browser history checkbox
+                history_selected = browsing_history_checkbox.is_selected()
+
+                if history_selected is True:
+                    browsing_history_checkbox.click()
+
+                time.sleep(15)
+                # Clear cookies and cache
+                clear_data_button.click()
+
+            finally:
+                self.selenium_webdriver.close()
 
     def check_for_browser_update(self):
         # Web scrape the latest version and compare
@@ -89,7 +118,7 @@ class BrowserFix:
             print("checking browser version on client", current_version_number)
             print("latest version on mac",latest_version['macos'])
 
-            if current_version_number == latest_version['macos']:
+            if current_version_number <= latest_version['macos']:
                 _LOGGER.info("Chrome browser up-to-date")
                 return True
             return False
@@ -99,9 +128,9 @@ class BrowserFix:
     def try_alternative_browser(self, browser_updated):
         """I can crash chrome - now I need to be able to see what crashed chrome and try it in another browser"""
         if browser_updated:
-            self.notification.create_notification_2("We have cleared your cache & cookies in Chrome. Your browser version is up to date.If problems persist, please try a new browser.", False)
+            self.notification.create_notification_2("Cache & cookies in Chrome have been cleared and browser is up to date. You may need try a different browser.", False)
         else:
-            self.notification.create_notification_2("We have cleared your cache & cookies in Chrome.Your browser version is out of date. Please update your browser.", False)
+            self.notification.create_notification_2("Cache & cookies in Chrome have been cleared. You need to update Chrome.", False)
 
 
 if __name__ == "__main__":
